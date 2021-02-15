c01f856c4565818518cb809c07f1b93df91c973a
Rick Brown <ricky.brown.00@gmail.com>
2021-02-15 11:19:18 +0000

    Initial Commit

---
+++ fitness-increment/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  fitness-increment
+//
+//  Created by Rick Brown on 15/02/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ fitness-increment/fitness_incrementApp.swift
@@ -0,0 +1,17 @@
+//
+//  fitness_incrementApp.swift
+//  fitness-increment
+//
+//  Created by Rick Brown on 15/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct fitness_incrementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
